06042f584c3afb339a9c2280ddb24bcc666a6600
SETH AGBAVITOR <sethagbavitor3@gmail.com>
2025-06-02 15:34:45 +0000

    Updated UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,57 +10,92 @@ namespace SimPaintRobot
         private PaintRobot robot;
         private static readonly Random delayRandom = new Random();
 
-        [TestInitialize]
-        public void Init()
-        {
-            robot = new PaintRobot(0.5); // Increased failure chance to 50% for stress simulation
-        }
-
-        private void RandomDelay()
-        {
-            int delay = delayRandom.Next(100, 2000);
-            Thread.Sleep(delay);
-        }
-
+        private void RandomDelay() => Thread.Sleep(delayRandom.Next(100, 2000));
         private void Delay30Seconds() => Thread.Sleep(30000);
         private void Delay60Seconds() => Thread.Sleep(60000);
         private void Delay120Seconds() => Thread.Sleep(120000);
 
-        [TestMethod] public void PaintABB5400_MotionRangeCheck() { Delay60Seconds(); Assert.IsTrue(robot.MoveTo(600, 300, 200)); }
-        [TestMethod] public void PaintIRB5500_ResetAfterFailure() { RandomDelay(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsTrue(robot.MoveTo(200, 200, 200)); }
-        [TestMethod] public void PaintIRB5400_SequenceError() { Delay30Seconds(); robot.MoveTo(300, 300, 300); robot.InjectRandomFailure(); Assert.IsFalse(robot.StartPainting()); }
-        [TestMethod] public void PaintIRB580_TriggerFailsafe() { RandomDelay(); robot.MoveTo(400, 200, 100); robot.InjectRandomFailure(); Assert.IsFalse(robot.MoveTo(100, 100, 100)); }
-        [TestMethod] public void ABBPaintingBot_RecoveryFromCrash() { Delay30Seconds(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsTrue(robot.StartPainting()); }
-        [TestMethod] public void SprayRobot_ResetWorks() { RandomDelay(); robot.InjectRandomFailure(); robot.Reset(); Assert.AreEqual((0, 0, 0), robot.CurrentPosition); }
-        [TestMethod] public void RoboticPainter_FailsafeBlocksPainting() { RandomDelay(); robot.InjectRandomFailure(); Assert.IsFalse(robot.StartPainting()); }
-        [TestMethod] public void ABBPainter_PaintStopsIfFailsafe() { Delay60Seconds(); robot.MoveTo(300, 200, 100); robot.StartPainting(); robot.InjectRandomFailure(); Assert.IsFalse(robot.StopPainting()); }
-        [TestMethod] public void PaintLineIRB_ValidPositioning() { RandomDelay(); Assert.IsTrue(robot.MoveTo(100, 500, 100)); }
-        [TestMethod] public void SmartPaintBot_PositionLimitFail() { RandomDelay(); Assert.IsFalse(robot.MoveTo(1200, 100, 100)); }
+        [TestMethod]
+        public void PaintABB5400_MotionRangeCheck()
+        {
+            robot = new PaintRobot(0.2); // 20% failure probability
+            Delay60Seconds();
+            Assert.IsTrue(robot.MoveTo(600, 300, 200));
+        }
 
-        [TestMethod] public void FlexPainter_RecoveryTest() { Delay30Seconds(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsTrue(robot.MoveTo(200, 300, 400)); }
-        [TestMethod] public void ABB5000_PaintCycle() { Delay60Seconds(); robot.MoveTo(250, 250, 250); if (robot.StartPainting()) Assert.IsTrue(robot.StopPainting()); }
-        [TestMethod] public void HighPrecisionArm_BoundaryFailure() { RandomDelay(); Assert.IsFalse(robot.MoveTo(-1, 0, 0)); }
-        [TestMethod] public void PaintIRBControl_FailureSimulation() { RandomDelay(); robot.InjectRandomFailure(); Assert.IsFalse(robot.MoveTo(500, 500, 500)); }
-        [TestMethod] public void ABB5500_ResetAndOperate() { RandomDelay(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsTrue(robot.StartPainting()); }
+        [TestMethod]
+        public void PaintIRB5500_ResetAfterFailure()
+        {
+            robot = new PaintRobot(0.5); // 50% failure probability
+            RandomDelay();
+            robot.InjectRandomFailure();
+            robot.Reset();
+            Assert.IsTrue(robot.MoveTo(200, 200, 200));
+        }
 
-        [TestMethod] public void SmartPainter_PositionPrecision() { RandomDelay(); Assert.IsTrue(robot.MoveTo(999, 999, 999)); }
-        [TestMethod] public void ABBFlexiblePaintBot_LowEdgeCheck() { RandomDelay(); Assert.IsTrue(robot.MoveTo(0, 0, 0)); }
-        [TestMethod] public void ABBPaintMaster_ResetClearsState() { RandomDelay(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsFalse(robot.IsInFailsafe); }
-        [TestMethod] public void ABBPaintBot_TriggerFailsafeFlow() { RandomDelay(); robot.MoveTo(100, 100, 100); robot.InjectRandomFailure(); Assert.IsTrue(robot.IsInFailsafe); }
-        [TestMethod] public void ABBPainter3000_StartStopValid() { RandomDelay(); robot.MoveTo(150, 150, 150); if (robot.StartPainting()) Assert.IsTrue(robot.StopPainting()); }
+        [TestMethod]
+        public void PaintIRB5400_SequenceError()
+        {
+            robot = new PaintRobot(0.4);
+            Delay30Seconds();
+            robot.MoveTo(300, 300, 300);
+            robot.InjectRandomFailure();
+            Assert.IsFalse(robot.StartPainting());
+        }
 
-        [TestMethod] public void ABBPainterX_CycleStopWithError() { Delay60Seconds(); robot.MoveTo(250, 150, 50); robot.StartPainting(); robot.InjectRandomFailure(); Assert.IsFalse(robot.StopPainting()); }
-        [TestMethod] public void PaintProSystem_ResetPosition() { RandomDelay(); robot.MoveTo(300, 200, 100); robot.InjectRandomFailure(); robot.Reset(); Assert.AreEqual((0, 0, 0), robot.CurrentPosition); }
-        [TestMethod] public void ABBAdvancedPaintBot_FailsafeMovement() { RandomDelay(); robot.InjectRandomFailure(); Assert.IsFalse(robot.MoveTo(400, 300, 200)); }
-        [TestMethod] public void ABBIRB6000_MovementBoundaries() { RandomDelay(); Assert.IsFalse(robot.MoveTo(2000, 0, 0)); }
-        [TestMethod] public void PaintBotEdgeTolerance_Check() { Delay30Seconds(); Assert.IsFalse(robot.MoveTo(1000, 1000, 1001)); }
-        [TestMethod] public void RoboticPaintX_MoveAndSpray() { RandomDelay(); robot.MoveTo(100, 200, 300); if (robot.StartPainting()) Assert.IsTrue(robot.StopPainting()); }
-        [TestMethod] public void PaintBotFaultyMove_ShouldFail() { RandomDelay(); Assert.IsFalse(robot.MoveTo(-100, -200, -300)); }
-        [TestMethod] public void IndustrialPaintBot_ValidStop() { RandomDelay(); robot.MoveTo(150, 250, 350); robot.StartPainting(); Assert.IsTrue(robot.StopPainting()); }
-        [TestMethod] public void MotionSystemCheck_FailureDuringRun() { Delay120Seconds(); robot.MoveTo(150, 200, 250); robot.StartPainting(); robot.InjectRandomFailure(); Assert.IsFalse(robot.StopPainting()); }
-        [TestMethod] public void ABBXPaintControl_ResetAndRetry() { RandomDelay(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsTrue(robot.MoveTo(50, 50, 50)); }
-        [TestMethod] public void CompactPaintBot_QuickTest() { RandomDelay(); Assert.IsTrue(robot.MoveTo(100, 100, 100)); }
-        [TestMethod] public void SimPaintRobot_ResetSequence() { RandomDelay(); robot.InjectRandomFailure(); robot.Reset(); Assert.IsTrue(robot.MoveTo(200, 200, 200)); }
+        [TestMethod]
+        public void PaintIRB580_TriggerFailsafe()
+        {
+            robot = new PaintRobot(0.6);
+            RandomDelay();
+            robot.MoveTo(400, 200, 100);
+            robot.InjectRandomFailure();
+            Assert.IsFalse(robot.MoveTo(100, 100, 100));
+        }
+
+        [TestMethod]
+        public void ABBPaintingBot_RecoveryFromCrash()
+        {
+            robot = new PaintRobot(0.3);
+            Delay30Seconds();
+            robot.InjectRandomFailure();
+            robot.Reset();
+            Assert.IsTrue(robot.StartPainting());
+        }
+
+        [TestMethod]
+        public void HighPrecisionArm_BoundaryFailure()
+        {
+            robot = new PaintRobot(0.8); // 80% failure probability for precision failure
+            RandomDelay();
+            Assert.IsFalse(robot.MoveTo(-1, 0, 0));
+        }
+
+        [TestMethod]
+        public void CompactPaintBot_QuickTest()
+        {
+            robot = new PaintRobot(0.1); // Low failure probability
+            RandomDelay();
+            Assert.IsTrue(robot.MoveTo(100, 100, 100));
+        }
+
+        [TestMethod]
+        public void SmartPainter_PositionPrecision()
+        {
+            robot = new PaintRobot(0.5);
+            RandomDelay();
+            Assert.IsTrue(robot.MoveTo(999, 999, 999));
+        }
+
+        [TestMethod]
+        public void ABBPaintBot_TriggerFailsafeFlow()
+        {
+            robot = new PaintRobot(0.7);
+            RandomDelay();
+            robot.MoveTo(100, 100, 100);
+            robot.InjectRandomFailure();
+            Assert.IsTrue(robot.IsInFailsafe);
+        }
     }
 
     public class PaintRobot
@@ -72,7 +107,7 @@ namespace SimPaintRobot
         private static Random rnd = new Random();
         private readonly double failureChance;
 
-        public PaintRobot(double failureChance = 0.5)
+        public PaintRobot(double failureChance)
         {
             this.failureChance = failureChance;
         }
